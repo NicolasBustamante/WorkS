b3f09530c0c09b449f4f63a93351a07513a0b44d
Nicolás <nicolasbustamantealfaro@gmail.com>
2019-07-18 04:05:46 -0400

    menu principal listo

--- app/src/main/java/com/example/works2/menu_principal.java
+++ app/src/main/java/com/example/works2/menu_principal.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class menu_principal extends Fragment {
@@ -23,7 +25,12 @@ public class menu_principal extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_menu_principal, container, false);
         context = rootView.getContext();
 
-        Button Post = (Button) rootView.findViewById(R.id.post);
+        Button Post = rootView.findViewById(R.id.post);
+        ImageButton contru = rootView.findViewById(R.id.construccion);
+        ImageButton electri = rootView.findViewById(R.id.electricidad);
+        ImageButton electro = rootView.findViewById(R.id.electronica);
+        ImageButton gasfi = rootView.findViewById(R.id.gasfiteria);
+
         Post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,7 +38,41 @@ public class menu_principal extends Fragment {
                 startActivity(intent);
             }
         });
+
+        contru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, contruccion_cat.class);
+                startActivity(intent);
+            }
+        });
+
+        electri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, electricidad_cat.class);
+                startActivity(intent);
+            }
+        });
+
+        electro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, electronica_cat.class);
+                startActivity(intent);
+            }
+        });
+
+        gasfi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, gasfiteria_cat.class);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
+
     }
 
 
